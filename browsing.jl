f70c397c784a72e726f66321b07b2864dcb20aee
渡部真伍 <s1170087@gmail.com>
2019-07-15 15:56:44 +0900

    clean:

--- browsing.jl
+++ browsing.jl
@@ -3,7 +3,7 @@ using PyCall
 const webdriver = pyimport("selenium.webdriver")
 
 
-browser = webdriver.Chrome(executable_path="./bin/chromedriver")
+browser = webdriver.Chrome(executable_path = "./bin/chromedriver")
 browser.get("https://www.yahoo.co.jp")
 sleep(2)
 browser.quit()
