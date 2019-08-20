18aff4f58e91907f2fa29aaf663e0cc11141aba9
渡部真伍 <s1170087@gmail.com>
2019-08-20 18:57:14 +0900

    add: regression

---
+++ regression/main.jl
@@ -0,0 +1,30 @@
+# 統計学入門 13章
+
+using Printf: @printf
+using Statistics: mean
+using CSV
+
+data = CSV.read("./data.csv")
+
+n = size(data, 1)
+X = data.前日の福岡の気圧
+Y = data.東京の気圧
+X̅ = mean(X)
+Y̅ = mean(Y)
+
+# @show X, X̅
+# @show X .- X̅
+# @show Y, Y̅
+# @show Y .- Y̅
+
+β̂₂ = sum((X .- X̅) .* (Y .- Y̅)) / sum((X .- X̅).^2)
+β̂₁ = Y̅ - β̂₂ * X̅
+Ŷ = β̂₁ .+ β̂₂ .* X
+ê = Y - Ŷ
+s² = sum(ê.^2 ./ (n - 2))
+s = sqrt(s²)
+η² = 1 - sum(ê.^2) / sum((Y .- Y̅).^2) # = sum((Ŷ - Y̅).^2) / sum((Y - Y̅).^2)
+
+@printf("標本回帰方程式[sample regression equation]\tŶ = %.2f + %.2fX\n", β̂₁, β̂₂)
+@printf("推定値の標準誤差[standard error of estimates]\ts.e. = %.2f\n", s)
+@printf("決定係数[coefficient of determination]\tη² = %.2f\n", η²)
